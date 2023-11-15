32e4149ca840695656c355eb6d9f59f1aaa6c54e
Emerson Okorie <emersonok52@gmail.com>
2023-11-15 13:19:14 -0300

    up

--- main.py
+++ main.py
@@ -1,27 +1,31 @@
 from bs4 import BeautifulSoup
 import requests
 
-response = requests.get("https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/")
+URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
+
+response = requests.get(URL)
 top_movies = response.text
 
+
+
 soup = BeautifulSoup(top_movies, "html.parser")
+# print(soup.prettify())
 movies = soup.find_all(name="h3", class_="title")
 
-all_movies = []
-for movie in movies:
-    a_movie = movie.getText()
-    all_movies.append(a_movie)
-    
-print(all_movies)
+# all_movies = []
+# for movie in movies:
+#     a_movie = movie.getText()
+#     all_movies.append(a_movie)
 
-movies = []
-cont = 1
-for i in all_movies:
-    tamanho = len(all_movies) - cont
-    movies.append(all_movies[tamanho])
-    cont += 1
+all_movies = [movie.getText() for movie in movies]
+best_movies = all_movies[::-1]
+
+with open ("top_100_movies.txt", "w") as file:
+    for movie in best_movies:
+        file.write(f"{movie}\n")
     
-print(movies)
+    
+
     
 
 
